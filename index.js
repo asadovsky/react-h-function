73369b126926d13e30d7ebce6a33f21a2af765ec
Adam Sadovsky <asadovsky@gmail.com>
2016-02-19 14:13:04 -0800

    use lodash.assign, since Object.assign is not widely supported

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var assign = require('lodash.assign');
+
 function isChildren(React, x) {
   return typeof x === 'string' || typeof x === 'number' || Array.isArray(x) || React.isValidElement(x);
 }
@@ -7,7 +9,7 @@ function h(React, selector, props, children) {
     children = props;
     props = {};
   }
-  props = Object.assign({}, props || {});
+  props = assign({}, props || {});
   console.assert(!props.id && !props.className);
   var parts = selector.split('.');
   var x = parts[0].split('#'), tagName = x[0], id = x[1];
